6cfd8d390263c8efe5edea1c59696d0d7ca31433
Huy Nguyen <ndhuy@fit.hcmus.edu.vn>
2025-03-20 14:23:19 +0000

    feat(session01): datatype part 01

---
+++ session_01_basic/bin/lab01_syntax_data_type.js
@@ -0,0 +1,4 @@
+"use strict";
+let isDone = false;
+console.log(isDone);
+//# sourceMappingURL=lab01_syntax_data_type.js.map
---
+++ session_01_basic/src/lab01_syntax_data_type.ts
@@ -0,0 +1,8 @@
+/**
+ * Base type in TypeScript
+ */
+
+//? boolead
+
+let isDone : boolean = false;
+console.log(isDone);
