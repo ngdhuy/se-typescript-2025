cdc44813fa3acfea26eacce76cfc4a2146849f31
Huy Nguyen <ndhuy@fit.hcmus.edu.vn>
2025-05-06 09:38:46 +0000

    refactor!: refactor code for factory method pattern

--- session_03_patterns/pattern_03_factory_method/src/ConcreteProductA2.ts
+++
@@ -1,7 +0,0 @@
-import { AbstractProductA } from './IAbstractProductA';
-
-export class ConcreteProductA2 implements AbstractProductA {
-    public usefulFunctionA(): string {
-        return 'The result of the product A2.';
-    }
-}
--- session_03_patterns/pattern_03_factory_method/src/IAbstractProductA.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Each distinct product of a product family should have a base interface. All
- * variants of the product must implement this interface.
- */
-export interface AbstractProductA {
-    usefulFunctionA(): string;
-}
